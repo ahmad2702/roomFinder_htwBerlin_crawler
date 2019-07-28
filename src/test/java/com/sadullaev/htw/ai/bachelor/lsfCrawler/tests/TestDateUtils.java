c9fb2a30db0b1975f919654064ae138db37dc93f
Akhmad Sadullaev <s.ahmad2702@gmail.com>
2019-07-28 15:58:56 +0200

    refactoring

--- src/test/java/com/sadullaev/htw/ai/bachelor/lsfCrawler/tests/TestDateUtils.java
+++ src/test/java/com/sadullaev/htw/ai/bachelor/lsfCrawler/tests/TestDateUtils.java
@@ -6,24 +6,15 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.sadullaev.htw.ai.bachelor.lsfCrawler.utils.DateUtil;
 
 public class TestDateUtils {
-	
-	private static final String START_DATE = "01.07.2019";
-	private static final String END_DATE = "07.07.2019";
-	
-	private static final String LSF_DATE_FORMAT = "01.07.2019";
-	private static final String SQL_DATE_FORMAT = "2019-07-01";
-	
-	private static List<String> dateListForFullPull = new ArrayList<String>(); 
-	private static List<String> dateListForUpdate= new ArrayList<String>(); 
-	
-	@BeforeClass
-	public static void initList() {
+
+	@Test
+ 	public void testDateListForFullPull() {
+		List<String> dateListForFullPull = new ArrayList<String>(); 
 		dateListForFullPull.add("01.07.2019");
 		dateListForFullPull.add("02.07.2019");
 		dateListForFullPull.add("03.07.2019");
@@ -32,6 +23,16 @@ public class TestDateUtils {
 		dateListForFullPull.add("06.07.2019");
 		dateListForFullPull.add("07.07.2019");
 		
+		String START_DATE = "01.07.2019";
+		String END_DATE = "07.07.2019";
+		
+		List<String> result = DateUtil.getDatesBetweenTwoDates(START_DATE, END_DATE);
+ 		assertTrue(result.equals(dateListForFullPull));
+ 	}
+	
+	@Test
+ 	public void testDateListForUpdate() {
+		List<String> dateListForUpdate= new ArrayList<String>(); 
 		LocalDate localDate = LocalDate.now();
 		for(int i = 0; i<7; i++) {
 			String day  = (localDate.getDayOfMonth() < 10)? ("0" + localDate.getDayOfMonth()) : ("" + localDate.getDayOfMonth());
@@ -40,17 +41,7 @@ public class TestDateUtils {
 			dateListForUpdate.add(day + "." + month + "." + localDate.getYear());
 			localDate = localDate.plusDays(1);
 		}
-	}
-	
-	
-	@Test
- 	public void testDateListForFullPull() {
-		List<String> result = DateUtil.getDatesBetweenTwoDates(START_DATE, END_DATE);
- 		assertTrue(result.equals(dateListForFullPull));
- 	}
-	
-	@Test
- 	public void testDateListForUpdate() {
+		
 		List<String> result = DateUtil.getDatesBetweenNowAndDate(dateListForUpdate.get(dateListForUpdate.size()-1));
  		assertTrue(result.equals(dateListForUpdate));
 		
@@ -58,6 +49,9 @@ public class TestDateUtils {
 	
 	@Test
  	public void testFormatConvertFromLsfToSql() {
+		String LSF_DATE_FORMAT = "01.07.2019";
+		String SQL_DATE_FORMAT = "2019-07-01";
+		
 		String result = DateUtil.getDateFormatForSql(LSF_DATE_FORMAT);
  		assertTrue(result.equals(SQL_DATE_FORMAT));
  	}
